265f09d18b73efcd36189f83b8d6c3bb01b32576
Alícia Antunes <antunesalicia07@gmail.com>
2024-05-10 14:24:29 +0100

    feat: change settings tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{css,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '1700': '1700px',
+      },
+      height: {
+        '720': '720px',
+      }
+    },
   },
   plugins: [],
 };
